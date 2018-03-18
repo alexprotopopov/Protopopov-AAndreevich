b411c1afaeb665b5695dcc90e157fedf3f734121
alexprotopopov <alexprov@bk.ru>
2018-03-18 17:44:59 +0500

    1.3. JavDoc. Code convetion

--- job4j/chapter_001/src/main/java/ru/job4j/Calculate.java
+++ job4j/chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,4 +1,14 @@
 package main.java.ru.job4j;
+/**
+*Class Класс для вычисление арифметических операций + - * / ^
+*@author alex protopopov
+*@since 18.03.2018
+*@version 1
+*/
+/**
+*Конструктов, вывод строки в консоль
+*@param arg - arg.
+*/
 public class Calculate {
 	public static void main(String[] args){
 		System.out.println("Hello World");
---
+++ job4j/chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alex Protopopov (alexprov@bk.ru)
+* @version $Id$
+* @since 0.1
+*/
+package main.java.ru.job4j;
